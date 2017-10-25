8e2b82d005f2b1088d4935fd161c2f759a85f411
Hareesh <hareeshbabu82ns@gmail.com>
2017-10-25 07:13:41 -0600

    router match params

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const EditExpensePage = () => (
+const EditExpensePage = props => (
   <div>
-    <p>Edit Expense</p>
+    <p>Edit Expense : {props.match.params.id}</p>
   </div>
 );
 
